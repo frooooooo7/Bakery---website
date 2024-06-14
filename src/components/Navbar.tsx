e8fd7fec186d379930c28e200a3a6631ad7c099e
Damian <froispro6969@gmail.com>
2024-06-14 17:49:35 +0200

    Added animation to threedots popup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,8 +32,8 @@ const Navbar = () => {
                 <div className="flex items-center relative">
                     <FontAwesomeIcon icon={faCartShopping} className='h-7' />
                     <FontAwesomeIcon icon={faEllipsisVertical} className='h-7 ml-3' onClick={toggleLoc} />
-                    {isLoc ?
-                        <div className='absolute top-12 right-0 bg-white shadow-lg p-4 rounded-lg w-72 flex'>
+                    
+                        <div className={`absolute top-12 right-0 bg-white shadow-lg p-4 rounded-lg w-72 flex transition-transform duration-300 ease-in-out ${isLoc ? 'transform opacity-100 scale-100' : 'transform opacity-0 scale-95'}`}>
                             <div>
                                 <FontAwesomeIcon icon={faLocationDot} style={{ color: "#f72293", }} className='h-9 w-9 mr-2'/>
                             </div>
@@ -42,10 +42,7 @@ const Navbar = () => {
                                 <p>38-200 Jasło</p>
                                 <button className='mt-2 px-4 py-2 bg-white text-black border-2 border-pink-500 rounded-lg font-bold active:bg-pink-500 active:text-white'>Get in Touch</button>
                             </div>
-
                         </div>
-                        :
-                        <div></div>}
                 </div>
             </div>
             <div className={`absolute top-15 left-0 h-full w-64 bg-white shadow-lg transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
