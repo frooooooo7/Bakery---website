0ee6eb6317a6dc56984ef5f2e80c12d0e5c239e5
Damian <froispro6969@gmail.com>
2024-06-21 13:28:11 +0200

    Updated Offer section

--- src/sections/Offer.tsx
+++ src/sections/Offer.tsx
@@ -5,6 +5,7 @@ import party_cupcakes from "../assets/party_cupcakes.jpg";
 import choco_cupcakes from "../assets/choco_cakes.webp";
 import weeding_cakes from "../assets/weeding_cakes.webp";
 
+
 const Offer = () => {
 
     const offers = [
@@ -26,29 +27,59 @@ const Offer = () => {
     ];
 
     const settings = {
-        dots: true,
+        className: "",
+        dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                }
+            }
+        ]
     };
 
     return (
         <section className="px-2 pt-10">
             <div className="container mx-auto">
                 <h1 className='text-center text-3xl font-bold pb-10'>CO OFERUJEMY</h1>
-                <Slider {...settings}>
-                    {offers.map((offer, index) => (
-                        <div key={index} className="flex flex-col items-center text-center">
-                            <div className='w-full h-80 flex items-center justify-center bg-gray-100 border border-gray-300 rounded-lg overflow-hidden'>
-                                <img src={offer.img} className='w-full h-full object-cover' />
+                <div className="slider-container">
+                    <Slider {...settings}>
+                        {offers.map((offer, index) => (
+                            <div className='p-4' key={index}>
+                                <div key={index} className="flex flex-col items-center text-center p-4 bg-gray-100 border border-gray-300">
+                                    <div className='w-full h-60 flex items-center justify-center'>
+                                        <img src={offer.img} className='w-full h-full object-cover' />
+                                    </div>
+                                    <h1 className='text-xl font-bold mt-4'>{offer.title}</h1>
+                                    <p className='mt-2 px-4'>{offer.description}</p>
+                                    <button className='mt-4 py-2 px-6 bg-pink-700 text-white rounded-full'>CZYTAJ WIĘCEJ</button>
+                                </div>
                             </div>
-                            <h1 className='text-xl font-bold mt-4'>{offer.title}</h1>
-                            <p className='mt-2 px-4'>{offer.description}</p>
-                            <button className='mt-4 py-2 px-6 bg-pink-700 text-white rounded-full'>CZYTAJ WIĘCEJ</button>
-                        </div>
-                    ))}
-                </Slider>
+                        ))}
+                    </Slider>
+                </div>
             </div>
         </section>
     );
