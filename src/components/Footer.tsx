28250bc98aa3a34582a8e6bb66bd6014a8605f44
Damian <froispro6969@gmail.com>
2024-07-22 17:29:06 +0200

    Updated the footer for larger screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,88 +16,92 @@ const Footer = () => {
     return (
         <section className="px-2 py-6 lg:py-12">
             <div className="container mx-auto">
-                <div className="gallery">
-                    <h1 className="text-center tracking-wider pb-10 text-xl font-bold">GALERIA</h1>
-                    <div className="grid grid-cols-3 gap-3">
-                        <img src={photo1} className="w-full h-full object-cover" />
-                        <img src={photo2} className="w-full h-full object-cover" />
-                        <img src={photo3} className="w-full h-full object-cover" />
-                        <img src={photo4} className="w-full h-full object-cover" />
-                        <img src={photo5} className="w-full h-full object-cover" />
-                        <img src={photo6} className="w-full h-full object-cover" />
+                <div className="lg:flex lg:justify-center lg:space-x-10 xl:space-x-28">
+                    <div className="gallery">
+                        <h1 className="text-center lg:text-left tracking-wider py-10 text-xl font-bold">GALERIA</h1>
+                        <div className="grid grid-cols-3 gap-3 sm:flex sm:justify-center lg:grid lg:grid-cols-3">
+                            <img src={photo1} className="w-full h-full sm:w-20 sm:h-20 lg:w-16 lg:h-16 xl:w-24 xl:h-24 object-cover" />
+                            <img src={photo2} className="w-full h-full sm:w-20 sm:h-20 lg:w-16 lg:h-16 xl:w-24 xl:h-24 object-cover" />
+                            <img src={photo3} className="w-full h-full sm:w-20 sm:h-20 lg:w-16 lg:h-16 xl:w-24 xl:h-24 object-cover" />
+                            <img src={photo4} className="w-full h-full sm:w-20 sm:h-20 lg:w-16 lg:h-16 xl:w-24 xl:h-24 object-cover" />
+                            <img src={photo5} className="w-full h-full sm:w-20 sm:h-20 lg:w-16 lg:h-16 xl:w-24 xl:h-24 object-cover" />
+                            <img src={photo6} className="w-full h-full sm:w-20 sm:h-20 lg:w-16 lg:h-16 xl:w-24 xl:h-24 object-cover" />
+                        </div>
                     </div>
-                </div>
 
-                <div className="links">
-                    <h1 className="text-center tracking-wider py-10 text-xl font-bold">SZYBKIE LINKI</h1>
-                    <div className="">
-                        <ol className="grid grid-cols-2 gap-3">
-                            <li className="flex items-center">
-                                <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
-                                <a href="">O Nas</a>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
-                                <a href="">Nasza Drużyna</a>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
-                                <a href="">Sklep</a>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
-                                <a href="">Ceny</a>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
-                                <a href="">FAQ</a>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
-                                <a href="">Skontaktuj Się</a>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
-                                <a href="">Co Oferujemy</a>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
-                                <a href="">Opinie</a>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
-                                <a href="">Szkoła Pieczenia</a>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
-                                <a href="">Nasze Przepisy</a>
-                            </li>
-                        </ol>
-                    </div>
-                </div>
+                    <div className="sm:grid sm:grid-cols-2 xl:gap-x-16">
+                        <div className="links">
+                            <h1 className="text-center lg:text-left tracking-wider py-10 text-xl font-bold">SZYBKIE LINKI</h1>
+                            <div className="sm:flex sm:flex-col sm:items-center">
+                                <ol className="grid grid-cols-2 gap-3 sm:flex sm:flex-col md:grid md:grid-cols-2 md:gap-x-14">
+                                    <li className="flex items-center">
+                                        <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
+                                        <a href="">O Nas</a>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
+                                        <a href="">Nasza Drużyna</a>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
+                                        <a href="">Sklep</a>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
+                                        <a href="">Ceny</a>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
+                                        <a href="">FAQ</a>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
+                                        <a href="">Skontaktuj Się</a>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
+                                        <a href="">Co Oferujemy</a>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
+                                        <a href="">Opinie</a>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
+                                        <a href="">Szkoła Pieczenia</a>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <span className="bg-gray-400 h-[2px] w-3 inline-block mr-2"></span>
+                                        <a href="">Nasze Przepisy</a>
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
 
-                <div className="in_touch mb-10">
-                    <h1 className="text-center tracking-wider pb-10 text-xl font-bold py-10">SKONTAKTUJ SIĘ Z NAMI</h1>
-                    <div className="flex flex-col text-center mb-5">
-                        <FontAwesomeIcon icon={faLocationDot} style={{ color: "#f72293", }} className="mb-2"></FontAwesomeIcon>
-                        <h1>Zygmunta Krasińskiego 55a</h1>
-                        <h1>38-200 Jasło</h1>
+                        <div className="in_touch flex flex-col items-center">
+                            <h1 className="text-center lg:text-left tracking-wider pb-10 text-xl font-bold py-10">SKONTAKTUJ SIĘ Z NAMI</h1>
+                            <div>
+                                <div className="flex flex-col text-center mb-5 md:flex-row md:gap-5">
+                                    <FontAwesomeIcon icon={faLocationDot} style={{ color: "#f72293", }}></FontAwesomeIcon>
+                                    <h1>Zygmunta Krasińskiego 55a <br></br>38-200 Jasło</h1>
+                                </div>
+                                <div className="flex flex-col items-center text-center mb-5 md:flex-row md:gap-5">
+                                    <FontAwesomeIcon icon={faPhone} style={{ color: "#f72293", }}></FontAwesomeIcon>
+                                    <h1>+49 123 123 123</h1>
+                                </div>
+                                <div className="flex flex-col items-center text-center mb-5 md:flex-row md:gap-5">
+                                    <FontAwesomeIcon icon={faEnvelope} style={{ color: "#f72293", }}></FontAwesomeIcon>
+                                    <h1>sweet_bakery@gmail.com</h1>
+                                </div>
+                                <div className="icons flex justify-center items-center gap-4">
+                                    <i className="w-10"><Facebook /></i>
+                                    <i className="w-10"><Twitter /></i>
+                                    <i className="w-10"><Instagram /></i>
+                                    <i className="w-10"><Google /></i>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex flex-col text-center mb-5">
-                        <FontAwesomeIcon icon={faPhone} style={{ color: "#f72293", }} className="mb-2"></FontAwesomeIcon>
-                        <h1>+49 123 123 123</h1>
-                    </div>
-                    <div className="flex flex-col text-center mb-5">
-                        <FontAwesomeIcon icon={faEnvelope} style={{ color: "#f72293", }} className="mb-2"></FontAwesomeIcon>
-                        <h1>sweet_bakery@gmail.com</h1>
-                    </div>
-                </div>
-
-                <div className="icons flex justify-center items-center gap-4">
-                    <i className="w-10"><Facebook /></i>
-                    <i className="w-10"><Twitter /></i>
-                    <i className="w-10"><Instagram /></i>
-                    <i className="w-10"><Google /></i>
                 </div>
             </div>
         </section>
