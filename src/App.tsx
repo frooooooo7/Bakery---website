7c3323e2ee4b953fbf95e87b1160f20523d3fe5d
Damian <froispro6969@gmail.com>
2024-07-25 19:39:06 +0200

    Added the routes and the shop page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Route, Routes  } from "react-router-dom"
 import Home from "./pages/Home/Home"
-
+import ShopPage from "./pages/Shop/ShopPage"
 
 function App() {
 
   return (
     <>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sklep" element={<ShopPage/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/pages/Shop/ShopPage.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/Navbar"
+import Shop from "./Shop"
+
+const ShopPage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Shop/>
+    </div>
+  )
+}
+
+export default ShopPage
