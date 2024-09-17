6588b5c36ba5c82f526f070b179d3dfa7c5cb905
Damian <froispro6969@gmail.com>
2024-09-17 14:32:50 +0200

    Modified ImageGallery component and errors handled

--- src/pages/Product/components/ImageGallery.tsx
+++ src/pages/Product/components/ImageGallery.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import error_img from "../../../assets/no_image.jpg"
 
 interface ImageGalleryProps {
     img: string[];
@@ -23,19 +24,31 @@ const ImageGallery = ({ img }: ImageGalleryProps) => {
 
     return (
         <div className="flex flex-col items-center max-w-2xl w-full">
-            <div className="w-[300px] mb-5">
-                <img src={mainImage} className="w-full h-full object-contain rounded-lg transition duration-500 ease-in-out" alt="Main" />
+            <div className="w-[300px] h-[350px] mb-5">
+                <img src={mainImage || error_img} className="w-full h-full object-contain rounded-lg transition duration-500 ease-in-out" alt="Main" />
             </div>
             <div className="flex justify-between mt-4 w-full gap-x-3">
-                {img.map((image, index) => (
-                    <div key={index} className={`w-1/3 ${activeIndex === index ? 'shadow-xl' : ''}`}>
+                    <div className={`w-1/3 ${activeIndex === 0 ? 'shadow-xl' : ''}`}>
                         <img
-                            src={image}
-                            className="w-full h-full bg-slate-50 p-3 cursor-pointer rounded-md transition-transform transform hover:scale-105 object-contain "
-                            onClick={() => handleImageClick(index)}
+                            src={img[0] || error_img}
+                            className="w-full h-full bg-slate-50 p-3 cursor-pointer rounded-md transition-transform transform hover:scale-105 object-contain"
+                            onClick={() => handleImageClick(0)}
                         />
                     </div>
-                ))}
+                    <div className={`w-1/3 ${activeIndex === 1 ? 'shadow-xl' : ''}`}>
+                        <img
+                            src={img[1] || error_img}
+                            className="w-full h-full bg-slate-50 p-3 cursor-pointer rounded-md transition-transform transform hover:scale-105 object-contain"
+                            onClick={() => handleImageClick(1)}
+                        />
+                    </div>
+                    <div className={`w-1/3 ${activeIndex === 2 ? 'shadow-xl' : ''}`}>
+                        <img
+                            src={img[2] || error_img}
+                            className="w-full h-full bg-slate-50 p-3 cursor-pointer rounded-md transition-transform transform hover:scale-105 object-contain"
+                            onClick={() => handleImageClick(2)}
+                        />
+                    </div>
             </div>
         </div>
     );
